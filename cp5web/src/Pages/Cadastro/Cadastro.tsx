7b0bb896aec4fff8facd25c220fe65fcddaef729
Nicolas <nicpaiffer@hotmail.com>
2024-04-19 09:14:35 -0300

    login e cadastro

--- cp5web/src/Pages/Cadastro/Cadastro.tsx
+++ cp5web/src/Pages/Cadastro/Cadastro.tsx
@@ -1,8 +1,47 @@
-const Cadastro = () =>{
-    return(
-        <div>
-            <h1> Cadastro</h1>
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Cadastro.css';
+import Footer from '../../Components/Footer/Footer';
+import Menu from '../../Components/Menu/Menu';
+
+const Cadastro: React.FC = () => {
+return (
+    <div className="signup-container">
+        <Menu/>
+        <div className="signup-content">
+        <h2>Cadastro</h2>
+        <form className="signup-form" onSubmit={handleSubmit}>
+            <label htmlFor="username">Usuário:</label>
+            <input type="text" id="username" name="username" />
+            <br />
+            <label htmlFor="email">E-mail:</label>
+            <input type="email" id="email" name="email" />
+            <br />
+            <label htmlFor="password">Senha:</label>
+            <input type="password" id="password" name="password" />
+            <br />
+            <button type="submit">Cadastrar</button>
+        </form>
+        <div className="signup-links">
+            <Link to="../Login">Já possui uma conta? Faça login</Link>
         </div>
-    )
+        </div>
+        <Footer/>
+    </div>
+    
+);
 }
+
+const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+event.preventDefault();
+const formData = new FormData(event.currentTarget);
+const username = formData.get('username') as string;
+const email = formData.get('email') as string;
+const password = formData.get('password') as string;
+
+console.log('Username:', username);
+console.log('Email:', email);
+console.log('Password:', password);
+}
+
 export default Cadastro
--- cp5web/src/Pages/Login/Login.tsx
+++ cp5web/src/Pages/Login/Login.tsx
@@ -1,9 +1,43 @@
-const Login = () =>{
-    return(
-        <div>
-            <h1>Login</h1>
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+import Footer from '../../Components/Footer/Footer';
+import Menu from '../../Components/Menu/Menu';
+
+const Login: React.FC = () => {
+return (
+    <div className="login-container">
+        <Menu/>
+        <div className="login-content">
+        <h2>Login</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+            <label htmlFor="username">Usuário:</label>
+            <input type="text" id="username" name="username" />
+            <br />
+            <label htmlFor="password">Senha:</label>
+            <input type="password" id="password" name="password" />
+            <br />
+            <button type="submit">Entrar</button>
+            <div className="login-links">
+            <Link to="/">Esqueci minha senha</Link>
+            <span> | </span>
+            <Link to="/cadastro">Cadastrar</Link>
+            </div>
+        </form>
         </div>
-    )
+        <Footer/>
+    </div>
+);
+}
+
+const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+event.preventDefault();
+const formData = new FormData(event.currentTarget);
+const username = formData.get('username') as string;
+const password = formData.get('password') as string;
+
+console.log('Username:', username);
+console.log('Password:', password);
 }
 
 export default Login
