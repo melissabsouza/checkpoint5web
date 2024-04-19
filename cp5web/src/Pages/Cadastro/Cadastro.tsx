5184a99a3a49aaeeafc74efbcaa2bb5b3166178a
Alissa S. Cezero <144124273+lissCez@users.noreply.github.com>
2024-04-19 08:38:24 -0300

    imports de componentes

--- cp5web/src/Pages/Cadastro/Cadastro.tsx
+++ cp5web/src/Pages/Cadastro/Cadastro.tsx
@@ -1,7 +1,12 @@
+import Footer from "../../Components/Footer/Footer";
+import Menu from "../../Components/Menu/Menu";
+
 const Cadastro = () =>{
     return(
         <div>
+            <Menu/>
             <h1> Cadastro</h1>
+            <Footer/>
         </div>
     )
 }
